9d5e9f4740d27b0e25c1f3fd98db77698556198a
zhengshuai <zhuanshuzs@163.com>
2018-01-08 22:38:39 +0800

    add singleton
    Signed-off-by: zhengshuai <zhuanshuzs@163.com>

---
+++ 4.单例模式/singleton.js
@@ -0,0 +1,19 @@
+/**
+ * 单例模式 singleton
+ * create by zhengshuai on 2018.1.8
+ */
+
+//  实现单例模式
+var Singleton = function (name) {
+    this.name = name
+    this.instance = null
+}
+Singleton.prototype.getName = function () {
+    console.log(this.name)
+}
+Singleton.getInstance = function () {
+    if (!this.instance) {
+        this.instance = new Singleton(name)
+    }
+    return this.instance
+}
